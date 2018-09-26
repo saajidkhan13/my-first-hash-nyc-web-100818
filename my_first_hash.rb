9c47d8e783b4702e4d120204b61ef23994155d11
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-26 21:45:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,14 +3,10 @@ end
 
 
 def shipping_manifest = {
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-"whale bone coreset" => 5, 
+  "whale bone coreset" => 5, 
 "porcelain vase" => 2, 
 "oil painting" => 3 
 }
-
 end
 
 def retrieval
