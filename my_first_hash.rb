84dd852a9fdee5d3e7471897668dfb522c78f6a3
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-26 21:38:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
-def my_hash
+def my_hash = {
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+  my_name => "Saajid", my_last_name = "Khan"
+  end
 
 
 def shipping_manifest
