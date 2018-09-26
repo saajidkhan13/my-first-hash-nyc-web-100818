fb913bb12127b33e005bfac095d3224fc09a4358
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-26 21:40:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
-def my_hash = {
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  "my_name" => "Saajid"}
+def my_hash = {"my_name" => "Saajid"}
 end 
-
+end 
 
 
 def shipping_manifest
